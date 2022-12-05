4f219c31fa6cecaead013b157de41de4bb8dcec8
Mikolaj <mikolajtchorz@gmail.com>
2022-12-05 23:12:23 +0100

    Konwersacja z klientem od strony pracownika

--- all_requests.php
+++ all_requests.php
@@ -14,6 +14,9 @@
 <body>
     <div class="block_request">
         <a id="centered_ahref" href="employee.php">Powrót na stronę</a>
+        <?php if (isset($_GET['fine'])) { ?>
+                <p class="fine"><?php echo $_GET['fine']; ?></p>
+        <?php } ?>
         <h1>Historia zgłoszeń</h1>
         <p class="requests">
             <?php 
@@ -26,12 +29,14 @@
                     <th> <font face="Arial">Opis problemu przez klienta</font> </th> 
                     <th> <font face="Arial">Odpowiedź pracownika</font> </th> 
                     <th> <font face="Arial">Status zgłoszenia</font> </th> 
+                    <th> <font face="Arial"></font> </th> 
                 </tr>';
 
                 $sql = "SELECT * FROM requests;";
                 $result = mysqli_query($conn, $sql);
                 //if($result = $conn->query($sql)){
                 while ($row = $result->fetch_assoc()) {
+                    $i=$row["id_request"];
                     $field1name = $row["date"];
                     $field2name = $row["client"];
                     $field3name = $row["subject"];
@@ -39,6 +44,8 @@
                     $field5name = $row["response"];
                     $field6name = $row["status"]; 
 
+                    ?><form id="modify_record" action="modify.php" method="POST">
+                        <input type="hidden" name="id_element" value="<?php echo $i; ?>"><?php
                     echo '<tr> 
                             <td>'.$field1name.'</td>
                             <td>'.$field2name.'</td> 
@@ -46,8 +53,10 @@
                             <td>'.$field4name.'</td> 
                             <td>'.$field5name.'</td> 
                             <td>'.$field6name.'</td> 
+                            <td>.<button type="submit">Modyfikuj odpowiedź</button></td> 
                         </tr>';
                     //echo $row['client']."<br>";
+                    ?></form><?php
                 }
                 $result->free();
                 //}
